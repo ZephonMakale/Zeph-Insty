baf05ee2510dab560beb5584b8e6b4011e5d5a87
ZephonMakale <mzephon@gmail.com>
2021-01-18 16:46:18 +0300

    Add the Post model

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    profile_image = models.ImageField(upload_to='posts/')
+    profile_name = models.TextField()
+    image = models.ImageField(upload_to='posts/')
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    image_caption = models.CharField(max_length= 100, blank=True)
+    timestamp = models.DateTimeField(default= timezone.now)
+    
+    
